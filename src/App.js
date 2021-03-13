1321c8c0ee635dfaf3640a8a1fc9de1f51a706ed
Reid Sherman <reidsherman@gmail.com>
2021-03-12 18:18:05 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+
 import logo from './logo.svg';
+import { useEffect, useState } from 'react'
+import Table from './table';
 import './App.css';
 
 function App() {
+  const [tableData, setTableData] = useState([])
+  useEffect(async () => {
+    const getTableData = async () => {
+      const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+      const json = await res.json()
+      setTableData(json)
+    }
+    getTableData()
+  }, [tableData])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Table tableRows={tableData}/>
         <a
           className="App-link"
           href="https://reactjs.org"
